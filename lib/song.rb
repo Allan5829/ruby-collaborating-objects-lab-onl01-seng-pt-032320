e33140fc7f3313262c8b9daf3d07e0b2d3345557
Allan Farca <allan.farca@gmail.com>
2020-05-01 05:29:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class Song 
   
   attr_accessor :artist, :name 
@@ -20,9 +21,29 @@ class Song
     song
   end
   
+=======
+class Song
+  attr_accessor :artist, :name
+
+  def initialize(name)
+    @name = name
+  end
+
+>>>>>>> 4f3c2c03071dc42d6958eb05023a2a1e350daac1
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
     artist.add_song(self)
   end
+<<<<<<< HEAD
   
 end 
+=======
+
+  def self.new_by_filename(file)
+    song_info = file.chomp(".mp3").split(" - ")
+    song = Song.new(song_info[1])
+    song.artist_name = song_info[0]
+    song
+  end
+end
+>>>>>>> 4f3c2c03071dc42d6958eb05023a2a1e350daac1
